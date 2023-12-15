641df301a116d6ddd13eba38de2002688bf15c0c
Maciej Lipski <maciej.lipski2.stud@pw.edu.pl>
2023-12-15 19:21:29 +0100

    Prezentacja1

---
+++ SpringApplication/src/test/java/bada_shelter/SpringApplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bada_shelter.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
